ddedb8856a4032798212db0bdf1c19af88f33684
Tyler <poland.tyler@gmail.com>
2017-08-07 22:17:12 -0600

    LoginVC UI

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,22 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by A664291 on 8/7/17.
+//  Copyright © 2017 KingdomBits. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by A664291 on 8/7/17.
+//  Copyright © 2017 KingdomBits. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+let TO_LOGIN = "toLogin"
