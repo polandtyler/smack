cb0a25e9bb090e0b72e5f6f8da4cc03fe3627ffc
Tyler <poland.tyler@gmail.com>
2017-08-09 19:57:25 -0600

    Adds auth service with register user request

--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -8,7 +8,20 @@
 
 import Foundation
 
-// Segues
+
+// ALIAS
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+// SEGUE
 let TO_LOGIN = "toLogin"
 let TO_CREATE = "toCreate"
 let UNWIND = "unwindToChannel"
+
+// USER DEFAULTS
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
+
+// URL
+let BASE_URL = "https://smackymcchat.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
