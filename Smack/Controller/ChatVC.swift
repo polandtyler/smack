7a4f5837b1f0c271dbc61889930e52bd3f6cf909
Tyler <poland.tyler@gmail.com>
2017-08-14 23:10:38 -0600

    Messages UI

--- Smack/Controller/ChatVC.swift
+++ Smack/Controller/ChatVC.swift
@@ -8,17 +8,24 @@
 
 import UIKit
 
-class ChatVC: UIViewController {
+class ChatVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     
     // OUTLETS
     @IBOutlet weak var menuBtn: UIButton!
     @IBOutlet weak var channelNameLbl: UILabel!
     @IBOutlet weak var messageTxtBox: UITextField!
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.bindToKeyboard()
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.estimatedRowHeight = 80
+        tableView.rowHeight = UITableViewAutomaticDimension
+        
         let tap = UITapGestureRecognizer(target: self, action: #selector(ChatVC.handleTap))
         view.addGestureRecognizer(tap)
         
@@ -73,7 +80,9 @@ class ChatVC: UIViewController {
         guard let channelId = MessageService.instance.selectedChannel?.id else { return }
         
         MessageService.instance.findAllMessagesForChannel(channelId: channelId) { (success) in
-            // something here
+            if success {
+                self.tableView.reloadData()
+            }
         }
     }
     
@@ -98,7 +107,24 @@ class ChatVC: UIViewController {
         }
     }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "messageCell", for: indexPath) as? MessageCell {
+            let message = MessageService.instance.messages[indexPath.row]
+            cell.configureCell(message: message)
+            
+            return cell
+        } else {
+            return UITableViewCell()
+        }
+    }
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return MessageService.instance.messages.count
+    }
     
     
     
---
+++ Smack/View/MessageCell.swift
@@ -0,0 +1,31 @@
+//
+//  MessageCell.swift
+//  Smack
+//
+//  Created by A664291 on 8/14/17.
+//  Copyright © 2017 KingdomBits. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+    
+    // Outlets
+    @IBOutlet weak var userImg: CircleImage!
+    @IBOutlet weak var usernameLbl: UILabel!
+    @IBOutlet weak var timestampLbl: UILabel!
+    @IBOutlet weak var messageBodyLbl: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    func configureCell(message: Message) {
+        messageBodyLbl.text = message.message
+        usernameLbl.text = message.username
+        userImg.image = UIImage(named: message.userAvatar)
+        userImg.backgroundColor = UserDataService.instance.returnUIColor(components: message.userAvatarColor)
+    }
+
+}
